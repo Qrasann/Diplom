d3ec5bf939dd6a83cc042a4a0d35fb79c82f5d1b
RomaQrasan <68272489+Qrasann@users.noreply.github.com>
2024-11-05 01:21:22 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,9 +3,7 @@ import pandas as pd
 
 
 def load_data():
-  # Путь к файлу data.csv в папке data
   data_path = os.path.join(os.path.dirname(__file__), 'data', 'data.csv')
 
-  # Загрузка данных
   data = pd.read_csv(data_path)
   return data
